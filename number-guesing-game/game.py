f8fc6bfaf7ee9aab1a57f62e54f3210d27eba5fb
Abhishek Bhatt <abhibhatt587@gmail.com>
2025-05-19 16:58:28 +0530

    Update game.py

--- number-guesing-game/game.py
+++ number-guesing-game/game.py
@@ -15,17 +15,17 @@ def number_guessing_game():
         try: 
             guess = int(input("Enter you guessing Number: "))
             attempt += 1 
-
+            
             if guess < number_to_guess: 
                 print("It's to Low! Try again.")
             elif guess > number_to_guess: 
                 print("It's to Hign! Try again.")
-            elif guess == number_guessing_game: 
+            else:
                 print(f"🎉 Congratulations! You guessed it in {attempt} attempts.")
-
+                break
         except ValueError:
             print("⚠️ Please enter a valid number.")
-
+           
 
 # Run the game while calling the function.
 number_guessing_game()
