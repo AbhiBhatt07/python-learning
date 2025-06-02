a734b7744cb77be7216b4cd8f2c9b256df722c8e
Abhishek Bhatt <abhibhatt587@gmail.com>
2025-06-02 14:58:11 +0530

    Make new url shortener small project.

---
+++ urlshortner/url.py
@@ -0,0 +1,21 @@
+import pyshorteners
+import pyperclip
+
+def shorten_url(long_url):
+    shortener = pyshorteners.Shortener()
+
+    short_url = shortener.tinyurl.short(long_url)
+
+    return short_url
+
+user_url = input("Enter your URL: ")
+
+try: 
+    short_url = shorten_url(user_url)
+    print("Shortend URL: ", short_url)
+
+    pyperclip.copy(short_url)
+    print("Shortend URL copied to clipboard.")
+
+except Exception as e: 
+    print("❌ An error occurred:", e)
